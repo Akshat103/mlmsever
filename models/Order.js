1e7fea8cb781e6955c86780f5ac411b672c5b975
Akshat103 <akshattamrakar103@gmail.com>
2024-10-17 16:23:20 +0530

    size fix order added in model

--- models/Order.js
+++ models/Order.js
@@ -4,7 +4,8 @@ const OrderSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     products: [{
         product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-        quantity: { type: Number, required: true }
+        quantity: { type: Number, required: true },
+        size: { type: String, required: false }
     }],
     totalAmount: { type: Number, required: true },
     totalPoints: { type: Number, required: true },
